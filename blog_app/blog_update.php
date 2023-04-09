01c5b5a4dfad7d2a7e507d587c8df1ea438a98b6
SEAL\pinot <a.hirano0109@gmail.com>
2023-04-10 01:21:22 +0900

    修正

--- blog_app/blog_update.php
+++ blog_app/blog_update.php
@@ -1,28 +1,34 @@
 <?php
 require_once('dbc.php');
+
 $blogs = $_POST;
+// $blogs = $_POST;
 
-if (empty($blogs['title'])){
-    exit('タイトルを入力してください');
-}
+// if (empty($blogs['title'])){
+//     exit('タイトルを入力してください');
+// }
 
-if (mb_strlen($blogs['title'] < 191)){
-    exit('タイトルは191文字以下にしてください');
-}
+// if (mb_strlen($blogs['title'] < 191)){
+//     exit('タイトルは191文字以下にしてください');
+// }
 
-if (empty($blogs['content'])){
-    exit('本文を入力してください');
-}
+// if (empty($blogs['content'])){
+//     exit('本文を入力してください');
+// }
 
-if (empty($blogs['category'])){
-    exit('カテゴリーは必須です');
-}
+// if (empty($blogs['category'])){
+//     exit('カテゴリーは必須です');
+// }
 
-if (empty($blogs['publish_status'])){
-    exit('公開ステータスは必須です');
-}
-
+// if (empty($blogs['publish_status'])){
+//     exit('公開ステータスは必須です');
+// }
+try{
 $dbc = new Dbc();
 $dbc->blogUpdate($blogs);
+$message = '更新しました';
 
+}catch(Exception $e){
+    $message = 'エラーが発生しました';
+}
 ?>
--- blog_app/update_form.php
+++ blog_app/update_form.php
@@ -1,7 +1,7 @@
 <?php
 require_once('dbc.php');
 $dbc = new Dbc();
-$id= $_GET['id'];
+$id = $_GET['id'];
 $result = $dbc->getABlog($id);
 
 $id = $result['id'];
@@ -12,7 +12,8 @@ $publish_status = (int)$result['publish_status'];
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,29 +22,42 @@ $publish_status = (int)$result['publish_status'];
     <link href="form.css" rel="stylesheet" type="text/css" />
 
 </head>
+
 <body>
     <div class="wrapper">
-    <h2>ブログ更新フォーム</h2>
-    <form action="blog_update.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $id?>">
-        <p>ブログタイトル：</p>
-        <input type="text" name="title" value="<?php echo $title?>">
-        <p>日時</p>
-        <input type="date" name="post_at" value="<?php echo $post_at?>">
-        <p>ブログ本文：</p>
-        <textarea name="content" id="content" cols="30" rows="10"><?php echo $content?></textarea>
-        <br>
-        <p>カテゴリ：</p>
-        <select name="category">
-            <option value="1" <?php if($category === 1) echo "selected"?>>日常</option>
-            <option value="2" <?php if($category === 2) echo "selected"?>>プログラミング</option>
-        </select>
-        <br>
-        <input type="radio" name="publish_status" value="1"  <?php if($publish_status === 1) echo "checked"?>>公開
-        <input type="radio" name="publish_status" value="2" <?php if($publish_status  === 2) echo "checked"?>>非公開
-        <br>
-        <input type="submit" value="送信">
-    </form>
+        <head>
+            <h2>ブログ更新フォーム</h2>
+        </head>
+        <main>
+            <form action="blog_update.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id ?>">
+                <label for="title">タイトル</label>
+                <input type="text" name="title" id="title" value="<?php echo $title ?>" required>
+                <label>
+                    <p>本文：</p>
+                    <textarea name="content" id="content" cols="30" rows="10" required><?php echo $content ?></textarea>
+                </label>
+                <label>
+                    <p>カテゴリ：</p>
+                    <select name="category">
+                        <option value="main" <?php if ($category == 'main') echo "selected" ?>></option>
+                        <option value="b" <?php if ($category == 'b') echo "selected" ?>></option>
+                    </select>
+                </label>
+                <label>
+                    <p>日時</p>
+                    <input type="date" name="post_at" value="<?php echo $post_at ?>" required>
+                </label>
+                <div class="publish_status">
+                    <p>公開設定</p>
+                    <label><input type="radio" name="publish_status" value="1" <?php if ($publish_status === 1) echo "checked" ?>>公開</label>
+                    <label><input type="radio" name="publish_status" value="2" <?php if ($publish_status  === 2) echo "checked" ?>>非公開</label>
+                </div>
+                <input type="submit" value="送信">
+            </form>
     </div>
+    <span><?php echo $message; ?></span>
+    </main>
 </body>
+
 </html>
